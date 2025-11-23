c63cb0635032c2916067da93c9ad9ed397316685
Navya Sinha <sinhanavya1875@gmail.com>
2025-11-23 11:33:27 +0530

    deployed be

--- frontend/sm-fe/src/api.js
+++ frontend/sm-fe/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api" || "https://stock-be.navyasinha.xyz/api",
+  baseURL: "https://stock-be.navyasinha.xyz/api",
 });
 
 export default api;
